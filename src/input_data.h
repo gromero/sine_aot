88fd025abcbd4e13a150a1be3b1431b0d6153137
Gustavo Romero <gustavo.romero@linaro.org>
2021-12-17 12:19:22 +0000

    Initial import

---
+++ src/input_data.h
@@ -0,0 +1,8 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <dlpack/dlpack.h>
+// const size_t input_data_len = 10;
+// float input_data[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, };
+
+const size_t input_data_len = 1;
+float input_data[] = {1.0,};
---
+++ src/output_data.h
@@ -0,0 +1,10 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <dlpack/dlpack.h>
+// const size_t output_data_len = 10;
+// float output_data[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, };
+
+const size_t output_data_len = 1;
+float output_data[1]; //  = { 1.23, };
+
+
